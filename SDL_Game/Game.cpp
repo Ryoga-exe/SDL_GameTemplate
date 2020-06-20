3095d5700266fe5776a75aa021a6e17e53092ed2
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-06-21 00:24:16 +0900

    Add Game class

---
+++ SDL_Game/Game.cpp
@@ -0,0 +1,34 @@
+#include "Game.h"
+
+Game::Game()
+:m_window(nullptr)
+,m_renderer(nullptr)
+,m_isRunning(true)
+,m_ticksCount(0)
+{
+
+}
+
+bool Game::Initialize() {
+	return true;
+}
+
+void Game::Finalize() {
+	
+}
+
+void Game::RunLoop() {
+
+}
+
+void Game::ProcessInput() {
+
+}
+
+void Game::Update() {
+
+}
+
+void Game::Draw() {
+
+}
--- SDL_Game/Game.h
+++ SDL_Game/Game.h
@@ -1,2 +1,19 @@
 #pragma once
 #include "SDL/SDL.h"
+
+class Game {
+public:
+	Game();
+	bool Initialize();
+	void RunLoop();
+	void Finalize();
+private:
+	void ProcessInput();
+	void Update();
+	void Draw();
+
+	SDL_Window*   m_window;
+	SDL_Renderer* m_renderer;
+	Uint32 m_ticksCount;
+	bool m_isRunning;
+};
